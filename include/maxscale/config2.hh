13f258dbcca9fccf0abe3101852722b346662baf
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-02-20 17:54:41 -0500

    spelling: parameter
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- include/maxscale/config2.hh
+++ include/maxscale/config2.hh
@@ -2211,7 +2211,7 @@ protected:
      * - assumed not to be modified at runtime via admin interface
      *
      * @param pValue  Pointer to the parameter value.
-     * @param pParam  Pointer to paramter describing value.
+     * @param pParam  Pointer to parameter describing value.
      * @param onSet   Optional functor to be called when value is set (at startup).
      */
     template<class ParamType,
@@ -2228,7 +2228,7 @@ protected:
      *
      * @param pContainer  Pointer to the container containing the parameter.
      * @param pValue      Member pointer to the parameter value.
-     * @param pParam      Pointer to paramter describing value.
+     * @param pParam      Pointer to parameter describing value.
      * @param onSet       Optional functor to be called when value is set (at startup).
      */
     template<class ParamType, class ConcreteConfiguration, class Container>
@@ -2245,7 +2245,7 @@ protected:
      * @param pArray   Member pointer to array of containers containing parameter.
      * @param index    Index of container in array.
      * @param pValue   Member pointer to the parameter value.
-     * @param pParam   Pointer to paramter describing value.
+     * @param pParam   Pointer to parameter describing value.
      * @param onSet    Optional functor to be called when value is set (at startup).
      */
     template<class ParamType, class ConcreteConfiguration, class Container, int N>
